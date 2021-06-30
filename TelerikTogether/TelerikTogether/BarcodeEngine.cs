b66269e64741ba0c6478743a3853384ca765aae5
martinivanovit <martinivanov.it@gmail.com>
2021-07-01 00:17:32 +0300

    Initial commit

---
+++ TelerikTogether/TelerikTogether/BarcodeEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace TelerikTogether
+{
+    public static class BarcodeEngine
+    {
+        public static byte[] GetBarcodeImageFromText(string text)
+        {
+            return null;
+        }
+    }
+}
---
+++ TelerikTogether/TelerikTogether/RadMath.cs
@@ -0,0 +1,10 @@
+﻿namespace TelerikTogether
+{
+    public static class RadMath
+    {
+        public static double DaiPet()
+        {
+            return 5;
+        }
+    }
+}
