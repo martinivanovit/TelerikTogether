879af0e6d10d78ed07f4c59b4c5736be2fb098e8
martinivanovit <martinivanov.it@gmail.com>
2021-07-01 13:07:54 +0300

    Reshaped the code structure

---
+++ TelerikTogether.Barcode.Tests/BarcodeEngine.Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelerikTogether.Barcode.Tests
+{
+    [TestClass]
+    public class BarcodeEngine
+    {
+        [TestMethod]
+        public void TestSomething()
+        {
+
+        }
+    }
+}
